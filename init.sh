d795c45423a0c49454e339ca4f449bdc90d9b51d
Wenjie <calebjay@live.cn>
2016-11-06 00:59:27 +0800

    Create init.sh
    Install mac apps w/ brew cask

---
+++ init.sh
@@ -0,0 +1,8 @@
+
+# Install apps w/ **brew cask**
+-------------------------------
+
+# Install brew cask
+
+# Install mac apps
+sudo brew cask install shortcat seil synergy atom iterm2 alfred karabiner spectacle cheatsheet
